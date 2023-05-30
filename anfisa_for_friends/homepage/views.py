57c354048d70f9c8f2381b9137bcf122009e1690
Николай Бабиков <n.babikov92@gmail.com>
2023-05-30 12:38:24 +0300

    add filter in homepage

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from ice_cream.models import IceCream
+from django.db.models import Q
 
 
 def index(request):
     template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('id', 'title')
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+        ).filter(
+        is_on_main=True, is_published=True
+        ).order_by('title')[1:4]
     context = {
         'ice_cream_list': ice_cream_list,
     }
